f44ee9fd838b8ffc923a67111f31a0ec5871808a
Aidan Williams <aidanwilliams01@gmail.com>
2024-05-08 16:17:10 -0700

    add buy and restock buttons

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,12 +1,31 @@
 import React from "react";
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 function Item(props){
+  const [quantity, setCount] = useState(props.quantity);
+
+  function decrement() {
+    setCount(quantity => quantity - 1);
+    // logic to handle out-of-stock
+  }
+
+  function restock(event) {
+    event.preventDefault();
+    setCount(quantity => parseInt(quantity) + parseInt(event.target.quantity.value));
+    // alert('a')
+  }
+
   return (
     <React.Fragment>
       <h3>{props.name}</h3>
       <p><em>{props.description}</em></p>
-      <p>In stock: {props.quantity}</p>
+      <p>In stock: {quantity}</p>
+      <button onClick={decrement}>Buy</button>
+      <form onSubmit={restock}>
+        <input type='number' name='quantity' min="1" />
+        <button>Restock</button>
+      </form>
       <hr/>
     </React.Fragment>
   );
@@ -15,7 +34,7 @@ function Item(props){
 Item.propTypes = {
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  quantity: PropTypes.number.isRequired
+  quantity: PropTypes.string.isRequired
 };
 
 export default Item;
